983ef444305d828dba369c6840b99f4c95d6ba08
Sergey Chelak <sergey.chelak@gmail.com>
2025-08-13 23:07:27 +0300

    literal tokens

--- src/scanner.rs
+++ src/scanner.rs
@@ -27,6 +27,7 @@ impl Scanner {
     }
 
     pub fn scan_token(&mut self) -> ScannerResult<Token> {
+        self.skip_non_code();
         self.code_start_idx = self.code_current_idx;
 
         let Some(ch) = self.advance_char() else {
@@ -62,28 +63,109 @@ impl Scanner {
                 let cond = self.match_char('=');
                 self.condition_make_token(cond, GreaterEqual, Greater)
             }
-            _ => return Err(self.make_error("Unexpected character")),
+            '"' => self.advance_string_token()?,
+            x if x.is_ascii_digit() => self.advance_number_token()?,
+            x => return Err(self.make_error(&format!("Unexpected character '{x}'"))),
         };
         Ok(token)
     }
 
-    // fn is_at_end(&self) -> bool {
-    //     self.code_start_idx == self.code.len()
-    // }
+    fn skip_non_code(&mut self) {
+        while self.skip_whitespace() || self.skip_comment_line() {}
+    }
+
+    fn skip_whitespace(&mut self) -> bool {
+        let mut skipped = false;
+        while let Some(ch) = self.peek_char() {
+            if !ch.is_ascii_whitespace() {
+                break;
+            }
+            self.advance_char();
+            if ch == '\n' {
+                self.line += 1;
+            }
+            skipped = true;
+        }
+        skipped
+    }
+
+    fn skip_comment_line(&mut self) -> bool {
+        if (Some('/'), Some('/')) != (self.peek_char(), self.peek_next_char()) {
+            return false;
+        }
+        while let Some(ch) = self.peek_char() {
+            self.advance_char();
+            if ch == '\n' {
+                break;
+            }
+        }
+        true
+    }
+
+    fn peek_char(&self) -> Option<char> {
+        self.code.get(self.code_current_idx).cloned()
+    }
+
+    fn peek_next_char(&self) -> Option<char> {
+        self.code.get(self.code_current_idx + 1).cloned()
+    }
 
     fn advance_char(&mut self) -> Option<char> {
-        let ch = self.code.get(self.code_current_idx);
+        let ch = self.peek_char();
         if ch.is_some() {
             self.code_current_idx += 1;
         }
-        ch.cloned()
+        ch
+    }
+
+    fn advance_string_token(&mut self) -> ScannerResult<Token> {
+        while let Some(ch) = self.peek_char() {
+            match ch {
+                '"' => break,
+                '\n' => self.line += 1,
+                _ => {}
+            }
+            self.advance_char();
+        }
+        if self.peek_char().is_none() {
+            return Err(self.make_error("Unterminated string"));
+        }
+        self.advance_char();
+        Ok(self.make_token(TokenType::String))
+    }
+
+    fn advance_number_token(&mut self) -> ScannerResult<Token> {
+        while let Some(ch) = self.peek_char() {
+            if !ch.is_ascii_digit() {
+                break;
+            }
+            self.advance_char();
+        }
+
+        if Some('.') == self.peek_char()
+            && self
+                .peek_next_char()
+                .map(|ch| ch.is_ascii_digit())
+                .unwrap_or(false)
+        {
+            self.advance_char();
+        }
+
+        while let Some(ch) = self.peek_char() {
+            if !ch.is_ascii_digit() {
+                break;
+            }
+            self.advance_char();
+        }
+
+        Ok(self.make_token(TokenType::Number))
     }
 
     fn match_char(&mut self, expected: char) -> bool {
-        let Some(ch) = self.code.get(self.code_current_idx) else {
+        let Some(ch) = self.peek_char() else {
             return false;
         };
-        if *ch != expected {
+        if ch != expected {
             return false;
         }
         self.code_current_idx += 1;
@@ -175,6 +257,53 @@ mod test {
             let result = scanner.scan_token();
             assert!(result.is_ok());
             assert!(matches!(result.unwrap().t_type, TokenType::Eof));
+        }
+    }
+
+    #[test]
+    fn scan_skip_whitespace() {
+        let code = "\t. ,\n!".chars().collect::<Vec<_>>();
+
+        let mut scanner = Scanner::new(code);
+        assert_eq!(scanner.scan_token().unwrap().t_type, TokenType::Dot);
+        assert_eq!(scanner.scan_token().unwrap().t_type, TokenType::Comma);
+        assert_eq!(scanner.scan_token().unwrap().t_type, TokenType::Bang);
+    }
+
+    #[test]
+    fn scan_skip_comment_line() {
+        let code = "\n    // hello\n . // world\n   \t,"
+            .chars()
+            .collect::<Vec<_>>();
+        let mut scanner = Scanner::new(code);
+        assert_eq!(scanner.scan_token().unwrap().t_type, TokenType::Dot);
+        assert_eq!(scanner.scan_token().unwrap().t_type, TokenType::Comma);
+    }
+
+    #[test]
+    fn scan_literal_token() {
+        let code = "\"1234\"".chars().collect::<Vec<_>>();
+        let mut scanner = Scanner::new(code);
+        let token = scanner.scan_token().unwrap();
+        assert_eq!(token.t_type, TokenType::String);
+        assert_eq!(token.length, 6);
+    }
+
+    #[test]
+    fn scan_literal_token_unterminated() {
+        let code = "\"1234".chars().collect::<Vec<_>>();
+        let mut scanner = Scanner::new(code);
+        assert!(scanner.scan_token().is_err());
+    }
+
+    #[test]
+    fn scan_numbers() {
+        let cases = ["1", "123", "12.23"];
+        for case in cases {
+            let code = case.chars().collect::<Vec<_>>();
+            let mut scanner = Scanner::new(code);
+            let token = scanner.scan_token().unwrap();
+            assert_eq!(token.t_type, TokenType::Number);
         }
     }
 }
