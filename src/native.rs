7653c59ec7dba7556f2a36a572eda8de947df840
Sergey Chelak <sergey.chelak@gmail.com>
2025-08-28 19:42:10 +0300

    native stdio input function

--- src/native.rs
+++ src/native.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use crate::{NativeFn, NativeFunctionsProvider, Value};
 
 pub struct ProductionNativeFunctions;
@@ -7,6 +9,7 @@ impl NativeFunctionsProvider for ProductionNativeFunctions {
         vec![
             ("write".to_string(), native_write),
             ("writeln".to_string(), native_writeln),
+            ("readln".to_string(), native_read_line),
         ]
     }
 }
@@ -20,4 +23,22 @@ fn native_writeln(args: &[Value]) -> Value {
     native_write(args);
     println!();
     Value::Nil
+}
+
+/// args are prompt
+fn native_read_line(args: &[Value]) -> Value {
+    native_write(args);
+    if std::io::stdout().flush().is_err() {
+        return Value::Nil;
+    }
+
+    // Wait for user input
+    let stdin = std::io::stdin();
+    let mut input = String::new();
+    if stdin.read_line(&mut input).is_err() {
+        return Value::Nil;
+    }
+    // don't include eof character, maybe windows will produce \r\n
+    let len = input.len();
+    Value::text_from_str(&input[0..len - 1])
 }
